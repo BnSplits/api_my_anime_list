c6a012cabfc28a282554cdef8b3bf4bfeeacdaba
Bn Split <159204088+BnSplits@users.noreply.github.com>
2024-07-16 20:01:27 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,30 +1,34 @@
 const express = require('express');
-const fs = require('fs');
+const axios = require('axios');
 const app = express();
 const port = 3001;
 
 // Middleware pour analyser le corps des requêtes JSON
 app.use(express.json());
 
-// Fonction pour lire le fichier JSON
-const readDataFromFile = () => {
-    const data = fs.readFileSync("https://raw.githubusercontent.com/manami-project/anime-offline-database/master/anime-offline-database.json");
-    return JSON.parse(data);
+// Fonction pour lire le fichier JSON distant
+const readDataFromFile = async () => {
+    const response = await axios.get("https://raw.githubusercontent.com/manami-project/anime-offline-database/master/anime-offline-database.json");
+    return response.data;
 };
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 // Route pour rechercher des animes par titre
-app.get('/s', (req, res) => {
+app.get('/s', async (req, res) => {
     const title = req.query.title;
     if (!title) {
         return res.status(400).send('Le paramètre "title" est requis');
     }
 
-    const jsonData = readDataFromFile();
-    const results = jsonData.data.filter(anime => anime.title.toLowerCase().includes(title.toLowerCase()));
+    try {
+        const jsonData = await readDataFromFile();
+        const results = jsonData.data.filter(anime => anime.title.toLowerCase().includes(title.toLowerCase()));
 
-    res.json(results);
+        res.json(results);
+    } catch (error) {
+        res.status(500).send('Erreur lors de la lecture du fichier JSON');
+    }
 });
 
 app.listen(port, () => {
